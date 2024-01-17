011f24fcc00b4f62e46dfead27ac6e5c428081cf
tamaratosic98 <tosict98@gmail.com>
2024-01-17 02:54:16 +0100

    use hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
-  console.log(process.env.PUBLIC_URL);
   return (
     <main className="bg-slate-300/20 h-full">
-      <BrowserRouter
-        basename={
-          process.env.PUBLIC_URL || "https://tamaratosic98.github.io/tt-island"
-        }
-      >
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
@@ -18,7 +13,7 @@ const App = () => {
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </main>
   );
 };
