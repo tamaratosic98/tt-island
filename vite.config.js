afe5dc96ab119588f563678fe8d59c3c5e5759a3
tamaratosic98 <tosict98@gmail.com>
2024-01-17 02:19:53 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
     define: {
-      "process.env.SOME_KEY": JSON.stringify(env.SOME_KEY),
+      "process.env.PUBLIC_URL": JSON.stringify(env.PUBLIC_URL),
     },
-    plugins: [react()],
-    assetsInclude: ["**/*.glb"],
+    plugins: [react(), viteCompression()],
+    assetsInclude: ["**/*.jpg", "**/*.png", "**/*.svg", "**/*.gif", "**/*.glb"],
+    base: "./",
   };
 });
