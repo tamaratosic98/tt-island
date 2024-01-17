661be8173daf1a5b123a827ece10dd0654b945fd
tamaratosic98 <tosict98@gmail.com>
2024-01-17 01:34:24 +0100

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20 h-full">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter base="/">
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
@@ -13,7 +13,7 @@ const App = () => {
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </main>
   );
 };
