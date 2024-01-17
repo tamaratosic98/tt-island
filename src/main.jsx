f1767dd5bfd815c0e1407019b6cd4003784c096f
tamaratosic98 <tosict98@gmail.com>
2024-01-17 01:07:40 +0100

    try to fix :(

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+);
