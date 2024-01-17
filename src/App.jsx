2f2d905a8fa710888ed0609aeac4b5e7cfac84fe
tamaratosic98 <tosict98@gmail.com>
2024-01-17 02:45:24 +0100

    revert browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
+  console.log(process.env.PUBLIC_URL);
   return (
     <main className="bg-slate-300/20 h-full">
-      <Router>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
@@ -13,7 +14,7 @@ const App = () => {
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </main>
   );
 };
