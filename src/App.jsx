62611da81540c9ece08cc6daa71ccbc4084cfc2c
tamaratosic98 <tosict98@gmail.com>
2024-01-17 01:29:51 +0100

    use  strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20 h-full">
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
